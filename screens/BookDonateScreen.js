afe089a33b5ab2f0dbe3662d36d4e169f7a3224a
prithvijai01 <64646473+prithvijai01@users.noreply.github.com>
2020-06-12 18:03:05 +0530

    Add files via upload

---
+++ screens/BookDonateScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, Image, TouchableOpacity,TextInput, Alert } from 'react-native';
+export default class BookDonateScreen extends Component{
+    render(){
+        return(
+            <Text>
+                Book Donate
+            </Text>
+        )
+    }
+}
---
+++ screens/BookRequest.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, Image, TouchableOpacity,TextInput, Alert } from 'react-native';
+export default class BookRequest extends Component{
+    render(){
+        return(
+            <Text>
+              Book request
+            </Text>
+        )
+    }
+}
